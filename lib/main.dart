be50b96a98e9f94ea84e8afae843a1f7ad7289ea
jhc <jhc@gsitm.com>
2025-02-26 16:08:01 +0900

    Refactor: Change Month Labels to Numeric Values
    - Replaces the Korean month labels ("1월", "2월", etc.) with numeric values ("1", "2", etc.) in the `monthLabels` list.

--- lib/main.dart
+++ lib/main.dart
@@ -48,18 +48,18 @@ class _MembershipScreenState extends State<MembershipScreen> {
     0.77,
   ];
   final List<String> monthLabels = [
-    "1월",
-    "2월",
-    "3월",
-    "4월",
-    "5월",
-    "6월",
-    "7월",
-    "8월",
-    "9월",
-    "10월",
-    "11월",
-    "12월",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
   ];
   DateTime _selectedDate = DateTime.now();
   List<double> _animationHeights = []; //

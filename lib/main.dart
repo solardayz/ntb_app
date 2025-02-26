2c8f4e6a444a239dc5418b6295cbb55e384854e1
Juna <solardayz@naver.com>
2025-02-26 20:47:12 +0900

    오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MembershipApp extends StatelessWidget {
         primarySwatch: Colors.grey,
         scaffoldBackgroundColor: Colors.black,
       ),
-      home: MembershipScreen(),
+      home: const MembershipScreen(),
     );
   }
 }
@@ -62,30 +62,25 @@ class _MembershipScreenState extends State<MembershipScreen> {
     "12",
   ];
   DateTime _selectedDate = DateTime.now();
-  List<double> _animationHeights = []; //
+  List<double> _animationHeights = [];
 
   @override
   void initState() {
     super.initState();
-    _animationHeights = List.filled(
-      monthlyAttendanceData.length,
-      0.0,
-    ); // 초기 높이를 0으로 설정
+    _animationHeights = List.filled(monthlyAttendanceData.length, 0.0);
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    _startAnimations(); // 애니메이션 시작
+    _startAnimations();
   }
 
   void _startAnimations() {
     Future.delayed(Duration(milliseconds: 500), () {
-      // 0.5초 딜레이 후 애니메이션 시작
       setState(() {
         for (int i = 0; i < monthlyAttendanceData.length; i++) {
-          _animationHeights[i] =
-              monthlyAttendanceData[i] * 150; // 데이터 값까지 높이 설정
+          _animationHeights[i] = monthlyAttendanceData[i] * 150;
         }
       });
     });
@@ -133,129 +128,128 @@ class _MembershipScreenState extends State<MembershipScreen> {
         ),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 12.0),
-          child: Expanded(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundImage: NetworkImage(
-                        'https://pimg.mk.co.kr/meet/neds/2014/10/image_readtop_2014_1274942_14123176891560616.jpg',
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      '신종훈',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () => _selectDate(context),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[900],
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Text(
-                      formattedDate,
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
+          // Expanded 제거 – Column을 바로 사용
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: NetworkImage(
+                      'https://pimg.mk.co.kr/meet/neds/2014/10/image_readtop_2014_1274942_14123176891560616.jpg',
                     ),
                   ),
-                ),
-                SizedBox(height: 12),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildStatCircle("전체", overallAttendance, Colors.white),
-                    _buildStatCircle("월간", weeklyAttendance, Colors.white),
-                    _buildStatCircle(
-                      "주간",
-                      monthlyAttendance,
-                      Colors.grey[100]!,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: SizedBox(
-                    height: 200,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: List.generate(monthlyAttendanceData.length, (index) {
-                        return Expanded( // Expanded 위젯을 Row의 자식으로 직접 이동
-                          child: Padding( // Padding 위젯을 Expanded의 자식으로 이동
-                            padding: EdgeInsets.symmetric(horizontal: 0), // 필요에 따라 패딩 조정
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                AnimatedContainer(
-                                  duration: Duration(milliseconds: 1000),
-                                  curve: Curves.easeInOut,
-                                  height: _animationHeights[index],
-                                  width: 10,
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[400],
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                ),
-                                SizedBox(height: 4),
-                                Text(monthLabels[index], style: TextStyle(fontSize: 20, color: Colors.white)),
-                              ],
-                            ),
-                          ),
-                        );
-                      }),
+                  const SizedBox(width: 10),
+                  const Text(
+                    '신종훈',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  "월별 출석률",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
+                ],
+              ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () => _selectDate(context),
+                child: Container(
+                  padding:
+                  const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                ),
-                Spacer(),
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(12),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey[700],
-                    ),
-                    onPressed: () {},
-                    child: Text('마이엔티비', style: TextStyle(color: Colors.white)),
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(12),
-                  color: Colors.grey[900],
                   child: Text(
-                    'NTB © 2025',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 12, color: Colors.white),
+                    formattedDate,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildStatCircle("전체", overallAttendance, Colors.white),
+                  _buildStatCircle("월간", weeklyAttendance, Colors.white),
+                  _buildStatCircle("주간", monthlyAttendance, Colors.grey[100]!),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SizedBox(
+                  height: 200,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: List.generate(monthlyAttendanceData.length, (index) {
+                      return Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              AnimatedContainer(
+                                duration: Duration(milliseconds: 1000),
+                                curve: Curves.easeInOut,
+                                height: _animationHeights[index],
+                                width: 10,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[400],
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(monthLabels[index],
+                                  style: const TextStyle(
+                                      fontSize: 20, color: Colors.white)),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                "월별 출석률",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+              const Spacer(),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(12),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[700],
+                  ),
+                  onPressed: () {},
+                  child: const Text('마이엔티비',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const Spacer(),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(12),
+                color: Colors.grey[900],
+                child: const Text(
+                  'NTB © 2025',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 12, color: Colors.white),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -272,7 +266,7 @@ class _MembershipScreenState extends State<MembershipScreen> {
           percent: percent,
           center: Text(
             "${(percent * 100).toInt()}%",
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.white,
@@ -284,10 +278,10 @@ class _MembershipScreenState extends State<MembershipScreen> {
           animation: true,
           animationDuration: 1000,
         ),
-        SizedBox(height: 4),
+        const SizedBox(height: 4),
         Text(
           label,
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 20,
             color: Colors.white,
             fontWeight: FontWeight.bold,
